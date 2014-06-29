594b82789e63deaadea959c01ea45a5d9d47121b
Diogo Anjos <diogo.silva.anjos@tecnico.ulisboa.pt>
2014-06-29 17:48:28 +0100

    some refactoring

--- src/ModbusDriver.java
+++ src/ModbusDriver.java
@@ -3,6 +3,9 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 import modbus.master.ModbusMasterLib;
+import modbus.master.exception.ModbusCommunicationException;
+import modbus.master.exception.ModbusConnectionException;
+import modbus.master.exception.ModbusResponseException;
 
 
 /*
@@ -13,44 +16,50 @@ import modbus.master.ModbusMasterLib;
 public class ModbusDriver /* implements DeviceAPI */ {
 
     ModbusMasterLib master;
-    
-    // Metadata to configure master to read a given datapoint
-    int slaveId;        // energyMeterId (e.g. 2 = library meter)
-    int offsetRegister; // first modbus packet that we want to read (e.g. 1)
-    int lengthRegisters;// number of modbus packet registers that we want to read (3 = tri-phase consumption)
+    int modbusOffsetRegisters;  // first modbus packet that we want to read (e.g. 1)
+    int modbusLengthRegisters;  // number of modbus packet registers that we want to read (3 = tri-phase consumption)
     
     Poller poller;
-    BlockingQueue<String> metersReadyToBeReaded;
+    BlockingQueue<String> metersReadyToBeReaded; //receive events sent by poller
     
     
     public ModbusDriver(){
         master = new ModbusMasterLib();
-        poller = new Poller();
+        configModbusMaster("127.0.0.1",1,3);
+
         metersReadyToBeReaded = new LinkedBlockingQueue<String>();
+        
+        poller = new Poller();
         configPoller();
+
         (new PollerReaderThread()).start();
         (new ReadModbusmasterThread()).start();
     }
     
-    //configure ModbusMaster to read a given energy meter
-    public void configureEnergyMeter(String slaveAddress, int slaveId, int offsetRegister, int lengthRegisters){
-        master.createModbusTcpMaster(slaveAddress); // slave address (e.g. 127.0.0.1)
-        this.slaveId = slaveId;
-        this.offsetRegister = offsetRegister;
-        this.lengthRegisters = lengthRegisters;
-        
-    }
-    
-    public short[] readEnergyMeter(){
-        return master.readInputRegisters(slaveId, offsetRegister, lengthRegisters);
-    }
-    
-    private void configPoller(){
-        Map<String,Long> pollerSettings = (new ConfigFile()).getSettings(); 
-        for(String deviceID : pollerSettings.keySet()){
-            poller.addAddress(deviceID, pollerSettings.get(deviceID));
+    private void configModbusMaster(String address, int offset, int length){
+        try {
+            this.modbusOffsetRegisters = offset;
+            this.modbusLengthRegisters = length;
+            master.createModbusTcpMaster(address);
+        } catch (ModbusConnectionException e) {
+            System.out.println("Error connecting with modbus slave in the address "+address);
+            e.printStackTrace();
         }
     }
+    
+  
+    
+    private short[] readEnergyMeter(int slaveId){
+        try {
+            return master.readInputRegisters(slaveId, modbusOffsetRegisters, modbusLengthRegisters);
+        } catch (ModbusResponseException | ModbusCommunicationException e) {
+            //System.out.println("Error: Communication between modbus master and slave failed"); //TODO apagar isto de implementar a classe ReadModbusmasterThread
+            //e.printStackTrace(); //TODO
+            return null;
+        }
+    }
+    
+    
     
     private class ReadModbusmasterThread extends Thread{
         public ReadModbusmasterThread() {
@@ -61,9 +70,13 @@ public class ModbusDriver /* implements DeviceAPI */ {
             while(true){
                 try {
                     String meterToRead = metersReadyToBeReaded.take();
-                    //TODO codigo que  lê o meter:meterToRead
-                    //TODO codigo que envia isto para o ESPER
-                    //System.out.println("MeterToRead: "+meterToRead); //TODO  DEBUG
+//                    System.out.println("MeterToRead: "+meterToRead); //TODO  DEBUG
+                    //TODO(1) converter meterToRead:String para param:int do metodo readEnergyMeter  
+                    short[] result = readEnergyMeter(17); // 17 is a stub value
+                    if(result != null){
+                        //TODO(2) from  result[1,2,3] compute de total amount of consumed energy
+                        //TODO(3) send this value to Esper via DEVICE API
+                    }
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -90,42 +103,12 @@ public class ModbusDriver /* implements DeviceAPI */ {
         }
     }
     
-    //-----------------------------------------------------------------------
-//    private class TestThread implements Runnable{
-//
-//        @Override
-//        public void run() {
-//            for (int i = 1; i <= 20; i++) {
-//                try {
-//                    Thread.sleep(1000);
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
-//                String next = getAllAvailable(poller);
-//                if (next != null) {
-//                    System.out.println("..."+i+" Received: " + next);
-//                } else {
-//                    System.out.println("..."+i);
-//                }
-//            }
-//        }
-//        
-//        private  String getAllAvailable(Poller poller){
-//            String res = null;
-//            while(true){
-//                String next = poller.getNext(); 
-//                if(next == null){
-//                    return res;
-//                }else{
-//                    if(res == null){
-//                        res = next;
-//                    }else{
-//                        res = res +","+ next; 
-//                    }
-//                }
-//            }
-//        }
-//    } 
+    private void configPoller(){
+        Map<String,Long> pollerSettings = (new ConfigFile()).getSettings(); 
+        for(String deviceID : pollerSettings.keySet()){
+            poller.addAddress(deviceID, pollerSettings.get(deviceID));
+        }
+    }
     
     
 }
