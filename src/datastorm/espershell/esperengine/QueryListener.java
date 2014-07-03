17f624fb76ccdc90c681be654023193881c730c0
Diogo Anjos <diogo.silva.anjos@tecnico.ulisboa.pt>
2014-07-03 11:47:44 +0100

    implemented dont/printToFile, dont/printToTerminal

--- src/datastorm/espershell/esperengine/QueryListener.java
+++ src/datastorm/espershell/esperengine/QueryListener.java
@@ -1,5 +1,9 @@
 package datastorm.espershell.esperengine;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
 import com.espertech.esper.client.EventBean;
 import com.espertech.esper.client.UpdateListener;
 
@@ -11,9 +15,12 @@ public class QueryListener
         implements UpdateListener {
 
     private QueryMetadata qMD;
-
+    
+    
     public QueryListener(QueryMetadata metadata) {
         qMD = metadata;
+        initFile();
+    
     }
 
     @Override
@@ -22,10 +29,16 @@ public class QueryListener
             if(qMD.getPrintToTerminal()){
                 printOutputToTerminal(newEvents, "NEW");
             }
+            if(qMD.getPrintToFile()){
+                printOutputToFile(newEvents, "NEW");
+            }
         }
         if (oldEvents != null) {
             if(qMD.getPrintToTerminal()){
                 printOutputToTerminal(newEvents, "OLD");
+            }
+            if(qMD.getPrintToFile()){
+                printOutputToFile(newEvents, "OLD");
             }
         }
     }
@@ -41,7 +54,45 @@ public class QueryListener
         }
     }
 
-    private void printOutputToFile(){
-        //TODO implement this
+    
+    
+    private void printOutputToFile(EventBean[] events, String typeOfEvent) {
+        String res="\n";
+        try {
+            res += "Query " + qMD.getQueryID() + " OUTPUT " + typeOfEvent + " Events:";
+            for (EventBean eb : events) {
+                res = res + "\n| " + eb.getUnderlying() + "\n";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        try {
+            RandomAccessFile file = new RandomAccessFile(qMD.getOutputFilename(), "rws");
+            byte[] text = new byte[(int) file.length()];
+            file.readFully(text);
+            file.seek((int) file.length());
+            file.writeBytes(res + "\n");
+            file.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+    
+    private void initFile() {
+        try {
+            File f = new File(qMD.getOutputFilename());
+            f.delete();
+            RandomAccessFile file = new RandomAccessFile(qMD.getOutputFilename(), "rws");
+            byte[] text = new byte[(int) file.length()];
+            file.readFully(text);
+            file.seek((int) file.length());
+            file.writeBytes(qMD.getQueryStatement());
+            file.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    
 }
